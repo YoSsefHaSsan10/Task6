ccb2fbe617897b1674f6f376c29e16d1d8fe9c36
YoSsefHaSsan10 <167657887+YoSsefHaSsan10@users.noreply.github.com>
2024-11-16 22:46:35 +0200

    test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from addCalculator import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(3, 5), 8)
+        self.assertEqual(add(-2, -4), -6)
+        self.assertEqual(add(7, -3), 4)
+        self.assertEqual(add(0, 5), 5)
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
